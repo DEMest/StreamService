3a025afa61f1cb7ffd0a1ca4b3bb20619cce2aed
Daniel Smolin <danielsmolin5@gmail.com>
2026-04-12 14:51:00 +0700

    chore: remove mock events module, wire PrismaModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from "@nestjs/common";
 import { HealthController } from "./health/health.controller";
-import { EventsModule } from "./events/events.module";
 import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
-  imports: [EventsModule, PrismaModule],
+  imports: [PrismaModule],
   controllers: [HealthController],
 })
 export class AppModule {}
